91609774047c5a2cfd5a180c191509a027694876
vio-lin <846271633@qq.com>
2019-03-10 09:04:39 +0800

    帮德国留学师妹的TestTool

---
+++ src/main/java/com/violin/testTool/testCase/TestCase1.java
@@ -0,0 +1,202 @@
+package com.violin.testTool.testCase;
+
+
+import com.violin.testTool.annotation.UnitTest;
+import com.violin.testTool.annotation.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * lin
+ * 2019/1/12
+ * 846271633@qq.com
+ */
+@UnitTest
+public class TestCase1 {
+    @Test
+    public boolean testCase1(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }
+
+    @Test
+    public boolean testCase2(){
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试2 完成");
+        return true;
+    }
+
+    @Test
+    public boolean testCase3(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }
+    @Test
+    public boolean testCase4(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }
+    @Test
+    public boolean testCase5(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }
+    @Test
+    public boolean testCase6(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }
+
+    @Test
+    public boolean testCase7(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }
+
+    @Test
+    public boolean testCase8(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }
+    @Test
+    public boolean testCase9(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }
+    @Test
+    public boolean testCase10(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }
+    @Test
+    public boolean testCase11(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }
+    @Test
+    public boolean testCase12(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }
+    @Test
+    public boolean testCase134(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }
+    @Test
+    public boolean testCase14(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }
+    @Test
+    public boolean testCase15(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }
+    @Test
+    public boolean testCase16(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }  @Test
+    public boolean testCase17(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }  @Test
+    public boolean testCase18(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("测试1 完成");
+        return true;
+    }
+
+
+
+
+}
