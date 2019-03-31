175a7f3b0bc89aac747b177fab8724b63d5ce35c
vio-lin <846271633@qq.com>
2019-03-31 09:58:15 +0800

    稍微改一下 项目的结构

--- dubbo-test/src/main/java/com/violin/test/dubboThreadPool/AbstractTest.java
+++
@@ -1,9 +0,0 @@
-package com.violin.test.dubboThreadPool;
-
-import org.apache.dubbo.common.URL;
-
-public class AbstractTest {
-    protected static URL mockUrl() {
-        return new URL("dubbo","localhsot",81);
-    }
-}
