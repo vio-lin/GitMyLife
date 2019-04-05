9c859f450272face6fd2cc01a7bbab03a8cd2a28
vio-lin <846271633@qq.com>
2019-04-05 23:52:00 +0800

    添加一个readme.md 笔记本用不了远程也没有办法做开发 1. add netty code 10% 算研究阶段吧 2. 下一步 服务端 客户端。代理做proxy

---
+++ rpc-client/src/main/java/com/violin/rpc/server/ViolinHandlerAdapter.java
@@ -0,0 +1,24 @@
+package com.violin.rpc.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author lin
+ * Date: 2019-04-05
+ */
+public class ViolinHandlerAdapter extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // Discard the received data silently.
+        ((ByteBuf) msg).release(); // (3)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
